748d15f6b7d65695223f470e3eff7d232539ff93
Ian Hales <contact@ian-hales.com>
2022-01-16 18:22:26 +0000

    Remove stream username/password from logs

--- security_notifier/log_helper.py
+++ security_notifier/log_helper.py
@@ -1,16 +1,38 @@
 import logging
+import re
 from typing import Text, Optional
 
 LogLevel = int
 
 
+# Remove passwords from logs
+# https://stackoverflow.com/a/48503066/168735
+# CC BY-SA 4.0
+# No modifications except Python formatting
+class SensitiveFormatter(logging.Formatter):
+    """Formatter that removes sensitive information in urls."""
+
+    @staticmethod
+    def _filter(s):
+        return re.sub(r':\/\/(.*?)\@', r'://', s)
+
+    def format(self, record):
+        original = logging.Formatter.format(self, record)
+        return self._filter(original)
+
+
 def setup_logger(level: LogLevel = logging.INFO):
-    logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', level=logging.DEBUG)
+    fmt_template = '%(asctime)s %(levelname)s:%(message)s'
+    logging.basicConfig(format=fmt_template, level=logging.DEBUG)
     logging.getLogger().setLevel(level)
+    for handler in logging.getLogger().handlers:
+        handler.setFormatter(SensitiveFormatter(fmt_template))
 
 
 def get_logger(name: Text, level: Optional[LogLevel] = None) -> logging.Logger:
     logger = logging.getLogger(name)
+    for handler in logger.handlers:
+        handler.setFormatter(SensitiveFormatter())
     if level is not None:
         logger.setLevel(level)
     return logger
